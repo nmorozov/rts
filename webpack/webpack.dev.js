3d9cba86a50e51a5fbb2c1a3a963a6c17e94c447
Nikita Morozov <nikmorze@gmail.com>
2020-04-23 23:25:57 +0400

    initial commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const { prod_Path, src_Path } = require("./path");
+
+module.exports = {
+  entry: {
+    main: "./" + src_Path + "/index.ts",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, prod_Path),
+    filename: "[name].[chunkhash].js",
+  },
+  devtool: "source-map",
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: false,
+      template: "./" + src_Path + "/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
